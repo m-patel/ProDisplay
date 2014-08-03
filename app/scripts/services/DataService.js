fb7bf8639b008363302908a06f8b11af9bf59418
mpatel <p.mihir@live.com>
2014-08-03 14:47:31 -0400

    formatting

--- app/scripts/services/DataService.js
+++ app/scripts/services/DataService.js
@@ -8,13 +8,6 @@
         var infoRef = ref.child("appinfo"); // app info
         var itemKey = '';
 
-        var appInfo = [];
-
-//        infoRef.on('value', function(dataSnapshot) {
-//            appInfo.push(dataSnapshot.val());
-//            console.log(appInfo);
-//        });
-
         return {
             getKey: function () {
                 return itemKey;
@@ -35,8 +28,6 @@
                 dataRef.push(item); // add new item to firebase list
             },
             updateItem: function (key, item) {
-                // Get the Firebase reference of the item
-
                 dataRef.child(key).set({
                     name: item.name,
                     description: item.description,
@@ -45,10 +36,7 @@
                 });
             },
             removeItem: function (key) {
-                // Avoid wrong removing
                 if (key == undefined)return;
-
-                // Firebase: Remove item from the list
                 $firebase(dataRef).$remove(key);
             }
         };
